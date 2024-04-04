749b653b3277d912fb5cad29562beb2bcf52dd25
Rakesh Taninki <rakeshtaninki4u@gmail.com>
2024-04-04 08:56:23 +0530

    updates

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,8 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    print(event)
+    print("Data")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello World!')
